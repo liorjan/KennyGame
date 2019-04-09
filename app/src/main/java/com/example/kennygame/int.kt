f93d32176a3303d30f1107b091086edbefccdb97
liorjan <liorjan3@gmail.com>
2019-04-09 23:51:19 +0300

    newGame

---
+++ app/src/main/java/com/example/kennygame/int.kt
@@ -0,0 +1,5 @@
+package com.example.kennygame
+
+class int {
+
+}
---
+++ app/src/main/java/com/example/kennygame/p0.kt
@@ -0,0 +1,8 @@
+package com.example.kennygame
+
+object p0 {
+    operator fun div(i: Int): Any? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
