fd799a802f01bd0c2af7da14f5cfe135df1de5da
liorjan <liorjan3@gmail.com>
2019-04-10 16:59:51 +0300

    finish

--- app/src/main/java/com/example/kennygame/MainActivity.kt
+++ app/src/main/java/com/example/kennygame/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -20,60 +21,53 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         score = 0
 
         imageArray = arrayListOf(imageView,imageView2,imageView3,imageView4,imageView5,imageView6,imageView7,imageView8,imageView9)
 
         hideImages()
 
-
         object : CountDownTimer(10000,1000){
             override fun onFinish() {
-                timeText.text = "Time's off"
+                timeText.text = "Times off"
                 handler.removeCallbacks(runnable)
                 for (image in imageArray)
                     image.visibility = View.INVISIBLE
             }
 
             override fun onTick(millisUntilFinished: Long) {
-                timeText.text = "Time: " + p0 / 1000
+                val p0:Int = (millisUntilFinished/1000).toInt()
+                timeText.text = "Time: " + p0
             }
 
         }.start()
-
-
     }
 
     fun hideImages() {
 
         runnable = object : Runnable {
             override fun run() {
-
-                for (image in imageArray) {
+                //hide all images
+                for (image in imageArray)
+                {
                     image.visibility = View.INVISIBLE
                 }
-
+                //init random function
                 val random = Random
+                //random a number between 0 - 8
                 val index = random.nextInt(8 - 0)
+                //show selected image
                 imageArray[index].visibility = View.VISIBLE
-
+                //run this function again in 0.5 sec
                 handler.postDelayed(runnable,500)
-
             }
         }
 
-        handler.post { runnable }
-
-        for (image in imageArray) {
-            image.visibility = View.INVISIBLE
-        }
+        handler.post(runnable)
     }
 
-    fun increaseScore(view: view) {
+    fun increaseScore(view: android.view.View) {
         score++
-
         scoreText.text = "score: " + score
     }
-
 }
